fb6260156b462e46262a91ad6c3a36717ab9c274
zamrivertech <mohamadtasul8000@gmail.com>
2026-07-13 08:44:24 +0200

    Save current attempt at swap

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -6,9 +6,40 @@ def bubble_sort numbers
 
     count += 1
 
-    p "Start N: " + number.to_s
-    p "Next N: " + numbers[count].to_s
-    p "__________________________"
+    temp = 0
+    start_n = number
+    next_n = numbers[count]
+
+    #p "Start: #{start_n}"
+    #p "Next: #{next_n}"
+    #p count
+    #puts "_____________"
+
+    if start_n > next_n
+
+    p "Before Swap"
+    p "Start: #{start_n}"
+    p "Next: #{next_n}"
+    p count
+    puts "____________"
+      
+       temp = start_n
+       numbers[count - 1] = next_n
+       numbers[count] = temp
+       temp = 0
+
+      p "After Swap"
+      p "Start: #{start_n}"
+      p "Next: #{next_n}"
+      p count
+      puts "____________"
+
+      p numbers
+
+     end
+
+     break if count >= numbers.length
+    
     
   end
 
