808af97e5e68400dd22ece0c9ea61145f684dc84
zamrivertech <mohamadtasul8000@gmail.com>
2026-07-13 13:19:16 +0200

    Clean implementation and start again

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,51 +1,27 @@
 def bubble_sort numbers
 
-  count = 0
-
-  numbers.each do |number|
-
-    count += 1
-
-    temp = 0
-    start_n = number
-    next_n = numbers[count]
-
-    #p "Start: #{start_n}"
-    #p "Next: #{next_n}"
-    #p count
-    #puts "_____________"
-
-    if start_n > next_n
-
-    p "Before Swap"
-    p "Start: #{start_n}"
-    p "Next: #{next_n}"
-    p count
-    puts "____________"
-      
-       temp = start_n
-       numbers[count - 1] = next_n
-       numbers[count] = temp
-       temp = 0
-
-      p "After Swap"
-      p "Start: #{start_n}"
-      p "Next: #{next_n}"
-      p count
-      puts "____________"
-
-      p numbers
-
-     end
-
-     break if count >= numbers.length
-    
-    
-  end
-
-  p numbers
 
 end  
 
 bubble_sort([4,3,78,2,0,2])
 # => [0,2,2,3,4,78]
+
+# [4,3,78,2,0,2] 
+
+# [4,3,78,2,0,2] Pass 1 , 4 > 3? Y, Swap 1
+#  ^ ^
+
+# [3,4,78,2,0,2] Pass 1 , 4 > 78? N, Skip, Swap 1
+#    ^  ^
+
+# [3,4,78,2,0,2] Pass 1 , 78 > 2? Y, Swap 2
+#       ^ ^
+
+# [3,4,2,78,0,2] Pass 1 , 78 > 0? Y, Swap 3
+#         ^ ^
+
+# [3,4,2,0,78,2] Pass 1 , 78 > 2? Y, Swap 4
+#           ^ ^
+
+# [3,4,2,0,2,78] Pass 1 , 78 > 2? Y, Swap 4
+#           
